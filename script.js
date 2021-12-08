77dc34bcc8aef919dea3cd16f6b0ef2cb0eb0e7f
nrakeeb <nouha_rakeeb@hotmail.co.uk>
2021-12-08 15:37:34 +0000

    chore: function for local storage and use previous searches
    Authors;nr

--- script.js
+++ script.js
@@ -177,6 +177,69 @@ $(document).ready(function () {
       $("#forecast").append(forecastSquare);
     }
   }
+  function initLocalStorage() {
+    if (localStorage.getItem("prevCityWeatherSrch") === null) {
+      localStorage.setItem("prevCityWeatherSrch", "[]");
+    } else if (localStorage.getItem("prevCityWeatherSrch") === "[]") {
+      return;
+    }
+  }
 
+  var currentSrchHist = JSON.parse(localStorage.getItem("prevCityWeatherSrch"));
 
+  function renderLastCity(lastCity) {
+    var cityName = lastCity;
+    var apiKey = "6406ca836e96fe35d13d0645f945ad0b";
+    var queryURL3 =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      cityName +
+      "&cnt=5&units=imperial&appid=" +
+      apiKey;
+    $.ajax({
+      url: queryURL3,
+      method: "GET",
+    }).then(function (results) {
+      $(".hide").attr("class", "row");
+      var currentCityName = results.name;
+      $("#currentCityInfo").text(currentCityName + " ");
+      var currentCityLon = results.coord.lon;
+      var currentCityLat = results.coord.lat;
+      findWithCoords(currentCityLat, currentCityLon);
+      var currentCityDt = results.sys.sunrise;
+      dateConverter(currentCityDt);
+      var currentWethIcon = results.weather[0].icon;
+      weatherIcon(currentWethIcon);
+    });
+  }
 
+  function addToSearchHist(newCityName) {
+    initLocalStorage();
+    var currentSrchHist = JSON.parse(
+      localStorage.getItem("prevCityWeatherSrch")
+    );
+    currentSrchHist.unshift(newCityName);
+    localStorage.setItem(
+      "prevCityWeatherSrch",
+      JSON.stringify(currentSrchHist)
+    );
+    dispalySearchHist();
+  }
+  function dispalySearchHist() {
+    $("#searchHistory").text("");
+    var currentSrchHist = JSON.parse(
+      localStorage.getItem("prevCityWeatherSrch")
+    );
+    for (i = 0; i < currentSrchHist.length; i++) {
+      $("#searchHistory").append("<br>");
+      var citySrchBtn = $("<button>");
+      citySrchBtn.addClass("btn btn-info prvCity");
+      citySrchBtn.attr("type", "button");
+      citySrchBtn.attr("id", currentSrchHist[i]);
+      citySrchBtn.text(currentSrchHist[i]);
+      $("#searchHistory").append(citySrchBtn);
+      if ([i] > 5) {
+        return;
+      }
+    }
+  }
+});
