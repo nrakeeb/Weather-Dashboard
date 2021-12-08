5d50f33b880f911d705f21fc29ea66228035f1af
nrakeeb <nouha_rakeeb@hotmail.co.uk>
2021-12-08 15:24:16 +0000

    chore: get apikey and queryURL
    Authors:NR

--- script.js
+++ script.js
@@ -1,16 +1,33 @@
-var cities = document.getElementById('city')
-var pastSearch = document.getElementById('past-search')
-var searchedCity = document.getElementById('searched-city')
-var apiKey = 'cfef46a9be5508dca8cbd550d239da50'
-searchButton()
+$(document).ready(function () {
+  var searchBtn = $("#searchBtn");
 
-function searchButton(){
-    var button = document.getElementById('btn');
-    button.addEventListener("click", fetchWeather.value);
-}
+  initLocalStorage();
 
-function fetchWeather(city) {
-    fetch('https://api.openweathermap.org/data/2.5/weather?q=' + city + '&appid=cfef46a9be5508dca8cbd550d239da50')
-        .then(response => response.json())
-        .then(data => console.log(data))
-}
+  if (localStorage.getItem("prevCityWeatherSrch") != "[]") {
+    var currentSrchHist = JSON.parse(
+      localStorage.getItem("prevCityWeatherSrch")
+    );
+    renderLastCity(currentSrchHist[0]);
+  }
+
+  dispalySearchHist();
+
+  //get city coordinates
+  $(document).on("click", ".prvCity", function (e) {
+    e.preventDefault();
+    var cityName = $(this).attr("id");
+    var apiKey = "cfef46a9be5508dca8cbd550d239da50";
+    var queryURL =
+      "https://api.openweathermap.org/data/2.5/weather?q=" +
+      cityName +
+      "&cnt=5&units=imperial&appid=" +
+      apiKey;
+    $.ajax({
+      url: queryURL,
+      method: "GET",
+    }).then(function (results) {
+      weatherForecast(results);
+    });
+  });
+
+ 
